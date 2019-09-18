61f89288a91a00505446b0ff5af9c2a76b6f3665
polsala <polsalalidon@gmail.com>
2019-09-18 19:42:59 +0200

    Utils exception matrix func

--- utils/matrix_operations.py
+++ utils/matrix_operations.py
@@ -5,5 +5,10 @@ def create_matrix_from_iterable(n_rows, n_columns, iterable_l):
     if not isinstance(iterable_l, Iterable):
         raise Exception('You passed a non iterable!')
 
+    iterable_l = list(iterable_l)
+
+    if n_rows * n_columns != len(iterable_l):
+        raise Exception('That\s not possible ma friend %s X %s != %s' % (n_rows, n_columns, iterable_l))
+
     # Todo Implement
     return [['A'*n_rows] * n_columns]
