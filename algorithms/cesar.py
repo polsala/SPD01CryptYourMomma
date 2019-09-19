35c1c2849bef18dd48f4cc6c1b353472c157d4b1
polsala <polsalalidon@gmail.com>
2019-09-19 19:00:08 +0200

    cesar encrypt algorithm

--- algorithms/cesar.py
+++ algorithms/cesar.py
@@ -2,12 +2,20 @@
 
 
 class AveCesar(object):
-    def __init__(self, n, alphabet):
+    def __init__(self, n: int, alphabet: list or tuple):
         self.secret_number = n
         self.alphabet = alphabet
+        self.alpha_size = len(alphabet)
 
     def encrypt(self, phrase):
-        pass
+        return ''.join(
+            [
+                self.alphabet[(self.alphabet.index(ch) + self.secret_number) % self.alpha_size]
+                if ch in self.alphabet
+                else ch
+                for ch in phrase
+            ]
+        )
 
     def decrypt(self, phrase):
         pass
