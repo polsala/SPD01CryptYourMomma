2d50efdf6d82c5b264d5142688fa8011459ab437
polsala <polsalalidon@gmail.com>
2019-09-20 00:52:27 +0200

    Dict mapping function

--- algorithms/polybius.py
+++ algorithms/polybius.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
-from utils.matrix_operations import create_matrix_from_iterable
+from utils.matrix_operations import create_matrix_from_iterable, create_dict_mapping_from_iterable
 
 
 class PolYBius(object):
-    def __init__(self, rows, columns, alphabet):
+    def __init__(self, rows, rows_code_list, columns, columns_code_list, alphabet):
         self.matrix_rows = rows
         self.matrix_columns = columns
         self.alphabet = alphabet
-        self.matrix = create_matrix_from_iterable(rows, columns, alphabet)
+        self.matrix_decoder = create_matrix_from_iterable(rows, columns, alphabet)
+        self.matrix_encoder_dict = create_dict_mapping_from_iterable(
+            rows, rows_code_list, columns, columns_code_list,  alphabet
+        )
 
     def encrypt(self, phrase):
         pass
--- utils/matrix_operations.py
+++ utils/matrix_operations.py
@@ -13,5 +13,30 @@ def create_matrix_from_iterable(n_rows, n_columns, iterable_l):
     return list(zip(*[iter(iterable_l)]*n_rows))
 
 
+def create_dict_mapping_from_iterable(n_rows, rows_code_list, n_columns, columns_code_list, iterable_l):
+    if not isinstance(rows_code_list, Iterable) or not isinstance(columns_code_list, Iterable):
+        raise Exception('You passed a non iterable as rows or columns code list!')
+
+    rows_code_list = list(rows_code_list)
+    columns_code_list = list(columns_code_list)
+
+    if n_rows != len(rows_code_list):
+        raise Exception('Size of rows codes list is distinct than number of rows')
+
+    if n_columns != len(columns_code_list):
+            raise Exception('Size of columns codes list is distinct than number of rows')
+
+    list_of_tuples = create_matrix_from_iterable(n_rows, n_columns, iterable_l)
+
+    res_dict = {}
+
+    for i in range(0, n_rows):
+        for j in range(0, n_columns):
+            values_to_be_mapped = list_of_tuples[i][j]
+            res_dict[rows_code_list[i], columns_code_list[j]] = values_to_be_mapped
+
+    return res_dict
+
+
 def remove_duplicates_from_list(some_list: list or tuple):
     return list(dict.fromkeys(some_list))
