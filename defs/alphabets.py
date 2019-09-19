32d0429b86af3b1d8fba3b1993cc50d58a3a3d97
polsala <polsalalidon@gmail.com>
2019-09-20 00:09:14 +0200

    Add british alphabet 5x5 ij grouped

--- defs/alphabets.py
+++ defs/alphabets.py
@@ -16,3 +16,11 @@ BRITISH_ALPHABET_ALL = [
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
 ]
 
+BRITISH_ALPHABET_LOWER_POLYBIUS_5_X_5 = [
+    'a', 'b', 'c', 'd', 'e',
+    'f', 'g', 'h', ['i', 'j'], 'k',
+    'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u',
+    'v', 'w', 'x', 'y', 'z'
+]
+
