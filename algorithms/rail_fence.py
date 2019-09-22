e1d4d704db7de90e6f38baa32799863fcc88398b
polsala <polsalalidon@gmail.com>
2019-09-22 19:58:13 +0200

    Rail fance encrypt decrypt

--- algorithms/rail_fence.py
+++ algorithms/rail_fence.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from defs import alphabets
 
 
 class RailFence(object):
@@ -7,8 +6,33 @@ class RailFence(object):
         self.n_rails = rails
         self.alphabet = alphabet
 
+    def _generate_rail_fence_matrix(self, phrase):
+        phrase = list(phrase)
+
+        list_flatted = [[] for _ in range(self.n_rails)]
+
+        list_i = 0
+        direction = 1
+
+        for ch in phrase:
+            list_flatted[list_i].append(ch)
+
+            if (list_i == self.n_rails - 1 and direction > 0) or (list_i == 0 and direction < 0):
+                direction *= -1
+
+            list_i += direction
+
+        return [j for i in list_flatted for j in i]
+
     def encrypt(self, phrase):
-        pass
+        return ''.join(self._generate_rail_fence_matrix(phrase))
 
     def decrypt(self, phrase):
-        pass
+        n_columns = len(phrase)
+        res = [''] * n_columns
+
+        # _generate_rail_fence_matrix of range phrase returns position in array of phrase_size X rails
+        for ch, i in zip(phrase, self._generate_rail_fence_matrix(range(len(phrase)))):
+            res[i] = ch
+
+        return ''.join(res)
